c41abaaec861e69805e049be622193cc7c410416
hwngcm <hwngcm@gmai.com>
2018-05-25 13:56:40 +0700

    chap10
    filter chap 10
    
    fix pull
    
    fix size
    
    fix chap 10
    
    fix indent
    
    fix order

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,17 @@
 class User < ApplicationRecord
   attr_accessor :remember_token
   before_save :email_downcase
-  validates :name, presence: true, length: {maximum: Settings.user.name.max_name}
+  validates :name, presence: true,
+    length: {maximum: Settings.user.name.max_name}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: Settings.user.email.max_email},
-                    format: {with: VALID_EMAIL_REGEX},
-                    uniqueness: {case_sensitive: false}
+  validates :email, presence: true,
+    length: {maximum: Settings.user.email.max_email},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
   has_secure_password
-  validates :password, presence: true, length: {minimum: Settings.user.password.min_password}
+  validates :password, presence: true,
+    length: {minimum: Settings.user.password.min_password},
+    allow_nil: true
 
   def email_downcase
     email.downcase!
@@ -24,17 +28,22 @@ class User < ApplicationRecord
       SecureRandom.urlsafe_base64
     end
   end
-    def remember
-      remember_token = User.new_token
-      update_attributes remember_digest: User.digest(remember_token)
-    end
 
-    def authenticated? remember_token
-      return false if remember_digest.nil?
-      Bcrypt::Password.new(remember_digest).is_password? remember_token
-    end 
+  def remember
+    remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
 
-    def forget
-      update_attributes remember_digest: nil
-    end
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end 
+
+  def forget
+    update_attributes remember_digest: nil
+  end
+
+  def current_user? user
+    self == user
+  end
 end
